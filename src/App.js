a14b5f8c5082d723236523dff30cd25f6ba427d3
Tabarek Ayad <tabarek.ayad@gmail.com>
2021-02-10 00:20:28 +0300

    passing through a function

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
-import PassingProps from './components/PassingProps'
 
 function App() {
 
-  addition(c, v) {
-    return c + v ;
-  }
+  const PassingByFunc = props =>  <div>
+                                    <p>{props.title}</p>
+                                    <p>{props.nmbr}</p>
+                                    <p>{props.anArr[3]}</p>
+                                    <h1>{JSON.stringify(props.anArr)}</h1>
+                                    <p>{props.anObj.x}</p>
+                                    <h1>{JSON.stringify(props.anObj)}</h1>
+                                  </div>
   
-  const add = (x, y) => x + y;
+
+  // addition(c, v) {
+  //   return c + v ;
+  // }
+  
+  //const add = (x, y) => x + y;
+
+  const nuFunc = (n, m) => m + n;
+
   return (
     <div className="App">
       
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <PassingProps title = "Hello World" num = {5} myObj = {{ a: 2, b: 4}} myArr = {[1,2,3,4,5]}
-        myFunc1 = {add} myFunc2 = {(xx, yy) => xx + yy} myFunc3 = {addition}/>
+        {/*<PassingProps title= "Hello World" num= {5} myObj= {{ a: 2, b: 4}} myArr= {[1,2,3,4,5]}
+        myFunc1= {add} myFunc2= {(xx, yy) => xx + yy} myFunc3= {addition}/> */}
+        <PassingByFunc title = "Passing text" nmbr = {3} anArr = {[4,5,6,7]} anObj = {{x:10, y: 11}} inFunc = {nuFunc(2, 4)}/>
         
         <a
           className="App-link"
